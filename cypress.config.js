6dcd10277f00e2684bb60fc5b0244fc6da01745a
Gabriel Cano <jg-cf@outlook.com>
2024-12-12 09:38:57 +0100

    Intentando arreglar archivo cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,19 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      const { defineConfig } = require("cypress");
-      const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-      const addCucumberPreprocessorPlugin =
-        require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-      const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
-      module.exports = defineConfig({
-        e2e: {
-          setupNodeEvents(on, config) {
-            const bundler = createBundler({
-              plugins: [createEsbuildPlugin(config)],
-            });
-            on("file:preprocessor", bundler);
-            addCucumberPreprocessorPlugin(on, config);
-            return config;
-          },
-          specPattern: "cypress/e2e/**/*.feature",
-          baseUrl: "https://todomvc.com/examples/react/dist/#/",
-        },
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
       });
+      on("file:preprocessor", bundler);
+      addCucumberPreprocessorPlugin(on, config);
+      return config;
     },
+    specPattern: "cypress/e2e/steps/todo.feature",
+    baseUrl: "https://todomvc.com/examples/react/dist/#/",
   },
 });
